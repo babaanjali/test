921dd8a55b9dc200e7314ce872a7150faca71dab
Ratan khandekar <ratankhandear2@gmail.com>
2022-02-08 21:08:57 +0530

    file uploaded successfully

---
+++ todoapplication/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+       <h1> My To do Application</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ todoapplication/src/components/todoapp.jsx
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react'
+import React, {component} from 'react'
+
+class TodoApp extends Component{
+    render()
+     return(
+         <div className="TodoApp">
+         My to do app
+         </div>
+     )
+
+
+}
